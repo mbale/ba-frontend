36d3d9ba75d049a5bf49e9808ac5e5fc5aa4419f
Balazs Mocsai <me@bael.me>
2017-12-27 12:31:26 +0100

    added game page component

---
+++ pages/games/_slug/index.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Hello'
+})
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ pages/games/index.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Games'
+})
+</script>
+
+<style lang="stylus">
+
+</style>
