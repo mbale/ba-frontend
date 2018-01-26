c4709858c6925cc1c1b58a341cc4be317245ebfe
Balazs Mocsai <me@bael.me>
2018-01-26 09:27:53 +0100

    added bookmaker component
    Signed-off-by: Balazs Mocsai <me@bael.me>

---
+++ pages/bookmakers/_slug/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Bookmaker',
+  async asyncData ({ params }) {
+    console.log(params)
+  }
+})
+</script>
+
+<style>
+
+</style>
