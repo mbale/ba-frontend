5468b06ba862579a515af37b14fa905d21e2bfb0
Balazs Mocsai <me@bael.me>
2018-01-26 13:01:48 +0100

    bookmaker informations
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- pages/bookmakers/_slug/index.vue
+++ pages/bookmakers/_slug/index.vue
@@ -1,14 +1,106 @@
 <template>
-  <div>
-
+  <div class="bookmaker">
+    <div class="information">
+      <h1 class="header">
+        {{ bookmaker.name }}
+      </h1>
+      <h2 class="sub-header">
+        {{ bookmaker.founded }} - {{ bookmaker.headquarters }}
+      </h2>
+      <div class="content">
+        <div class="row row__image">
+          <img class="image" v-bind:src="bookmaker.logo">
+        </div>
+        <div class="row">
+          <span class="property">Description</span>
+        </div>
+        <div class="row">
+          <p class="text text__description">{{ bookmaker.description }}</p>
+        </div>
+        <div class="row">
+          <span class="property">Support</span>
+          <div class="text">
+            <a v-bind:href="`mailto:${bookmaker.supportEmail}?subject=support`">{{ bookmaker.supportEmail }}</a>
+          </div>
+        </div>
+        <div class="row">
+          <span class="property">URL</span>
+          <div class="text">
+            <a target="_blank" v-bind:href="bookmaker.url">{{ bookmaker.url }}</a>
+          </div>
+        </div>
+        <div class="row">
+          <span class="property">AffiliateURL</span>
+          <div class="text">
+            <a target="_blank" v-bind:href="bookmaker.affiliateUrl">{{ bookmaker.affiliateUrl }}</a>
+          </div>
+        </div>
+        <div class="row">
+          <span class="property">Licenses</span>
+          <p class="text text__licenses">
+            <span class="license" v-bind:key="license" v-for="license in bookmaker.licenses">
+              {{ license }}
+            </span>
+          </p>
+        </div>
+        <div class="row">
+          <span class="property">Bonuses</span>
+          <div class="text text__bonuses">
+            <span class="bonus" v-bind:key="bonus" v-for="bonus in bookmaker.bonuses">
+              <span>{{ bonus.title }}</span>
+              <img class="image" v-bind:src="getBonusIconURL(bonus.type)">
+            </span>
+          </div>
+        </div>
+        <div class="row">
+          <span class="property">Restricted Countries</span>
+          <p class="text text__country">
+            <span class="country" v-bind:key="country" v-for="country in bookmaker.restrictedCountries">{{ country }}</span>
+          </p>
+        </div>
+        <div class="row">
+          <span class="property">Deposit methods</span>
+          <div class="text text__deposit-methods">
+            <span class="license" v-bind:key="method.slug" v-for="method in bookmaker.depositMethods">
+              {{ method.name }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="reviews">
+      <h1 class="header">Reviews</h1>
+      <h2 class="sub-header">what you said about them</h2>
+      <div class="content">
+        <div class="review" v-bind:key="review.text" v-for="review of reviews">
+          {{ review.text }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import signupBIconURL from '~/assets/images/bonuses/signup-bonus.svg'
 
 export default Vue.extend({
   name: 'Bookmaker',
+  computed: {
+    bookmaker () {
+      return this.$store.state.bookmakers.bookmaker
+    },
+    reviews () {
+      return this.bookmaker.reviews.items
+    }
+  },
+  methods: {
+    getBonusIconURL (type) {
+      if (type === 'Signup') {
+        return signupBIconURL
+      }
+    }
+  },
   async asyncData ({ store, params }) {
     const slug = params.slug
 
@@ -19,6 +111,101 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style lang="stylus" scoped>
+
+.bookmaker
+  display flex
+  flex-direction column
+  max-width 1180px
+  margin 50px
+
+  .header
+    font-family "Open Sans", Helvetica, sans-serif
+    color #000
+    font-size 1.5em
+    margin-bottom 2px
+
+  .sub-header
+    font-size 1.1em
+    color #6c7a89
+    // border-bottom 1px solid #d2d7d3
+    margin-bottom 25px
+    padding-bottom 5px
+    font-weight 300
+
+  .information
+    display flex
+    flex-direction column
+    background-color white
+    margin-bottom 50px
+    padding 25px
+
+    .content
+      display flex
+      flex-direction column
+
+      .row
+        display flex
+        justify-content flex-start
+        margin-bottom 35px
+
+        &:nth-child(2)
+          margin-bottom 15px
+
+        &__image
+          align-self center
+
+        .image
+          max-width 100%
+          height auto
+
+        .property
+          color #99A7BC
+          font-weight 800
+          text-transform uppercase
+          margin-right auto
+
+        .text
+          color #4F5969
+
+          &__country
+            display flex
+            max-width 50%
+            flex-wrap wrap
+
+            .country
+              margin 5px
+
+          &__description
+            line-height 1.75
+            // max-width 75%
+
+          &__bonuses
+            display flex
+            flex-direction column
+
+            .bonus
+              display flex
+              flex-direction column
+              align-items center
+
+              .image
+                color inherit
+                align-self flex-end
+                max-width 15%
+                margin-top 20px
+
+          &__licenses
+            display flex
+            flex-direction column
+
+            .license
+              margin-bottom 10px
+
+
+
+  .reviews
+    background-color white
+    padding 20px
 
 </style>
