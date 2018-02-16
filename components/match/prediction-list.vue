104750dcbd9ad29f47457faa2fd181910eab5e19
Balazs Mocsai <me@bael.me>
2018-02-16 12:18:20 +0100

    same
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- components/match/prediction-list.vue
+++ components/match/prediction-list.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="panel">
     <div class="row" v-bind:key="prediction.id" v-for="prediction of predictions">
-      {{ prediction.text }}
+      <div class="user">
+        {{ getUser(prediction).username }}
+      </div>
+      <div class="prediction">
+        <span class="text">{{ prediction.text }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -12,6 +17,11 @@ export default {
   computed: {
     predictions () {
       return this.$store.state.match.data.predictions
+    }
+  },
+  methods: {
+    getUser (prediction) {
+      return prediction.user
     }
   }
 }
