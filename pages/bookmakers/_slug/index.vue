91f399b7f167eb305fca857c22a4f96a3a1fe6e7
Balazs Mocsai <me@bael.me>
2018-02-22 15:43:55 +0100

    typo
    Signed-off-by: Balazs Mocsai <me@bael.me>

--- pages/bookmakers/_slug/index.vue
+++ pages/bookmakers/_slug/index.vue
@@ -145,7 +145,7 @@ export default Vue.extend({
     },
     userCanSubmitReview () {
       const reviews = this.$store.state.bookmakers.bookmaker.reviews.items
-      const user = this.$store.state.user.profile
+      const user = this.$store.state.user
 
       if (!user) {
         return false
